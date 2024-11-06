c1e77a3bb80906c52518d798c56e9720b52882d2
iphydf <iphydf@users.noreply.github.com>
2024-11-06 16:39:50 +0000

    refactor: Migrate from ansi-wl-pprint to prettyprinter.
    The former is deprecated.

--- src/Apigen/Language/Apidsl.hs
+++ src/Apigen/Language/Apidsl.hs
@@ -1,154 +1,164 @@
 {-# OPTIONS_GHC -Wwarn #-}
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE Strict            #-}
-{- HLINT ignore "Functor law" -}
-{- HLINT ignore "Use <$" -}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE Strict     #-}
 module Apigen.Language.Apidsl where
 
-import           Apigen.Parser.SymbolTable    (Name, display, displayWithin)
-import           Apigen.Types                 (BitSize (..), BuiltinType (..),
-                                               Constness (..), Decl (..),
-                                               Generated (..), Model (..),
-                                               Module (..))
-import           Data.Maybe                   (maybeToList)
-import           Data.Text                    (Text)
-import qualified Data.Text                    as Text
-import           Language.Cimple              (Lexeme (..), lexemeText)
-import           Prelude                      hiding ((<$>))
-import           Text.PrettyPrint.ANSI.Leijen
+import           Apigen.Parser.SymbolTable (Name, display, displayWithin)
+import           Apigen.Types              (BitSize (..), BuiltinType (..),
+                                            Constness (..), Decl (..),
+                                            Generated (..), Model (..),
+                                            Module (..))
+import           Data.Maybe                (maybeToList)
+import           Data.Text                 (Text)
+import qualified Data.Text                 as Text
+import qualified Data.Text.Lazy            as TL
+import           Language.Cimple           (Lexeme (..), lexemeText)
+import           Prettyprinter
+import           Prettyprinter.Render.Text as Term
 
 type Context = [Text]
 
 alwaysNamespace :: Bool
 alwaysNamespace = True
 
-commaSpace :: Doc
+commaSpace :: Doc ()
 commaSpace = comma <> softline
 
-ppModel :: Model (Lexeme Name) -> Doc
+ppModel :: Model (Lexeme Name) -> Doc ()
 ppModel (Model mods) = vcat (map ppModule mods) <> line
 
-ppModule :: Module (Lexeme Name) -> Doc
+ppModule :: Module (Lexeme Name) -> Doc ()
 ppModule (Module file decls) =
-    text "from \"" <> text file <> "\"" <$> line <> vcat (map (ppDecl []) decls)
+    pretty "from \"" <> pretty (file <> "\"") <$$> line <> vcat (map (ppDecl []) decls)
 
-ppFunction :: Context -> Lexeme Name -> [Decl (Lexeme Name)] -> Doc
+ppFunction :: Context -> Lexeme Name -> [Decl (Lexeme Name)] -> Doc ()
 ppFunction ctx name params =
     ppLexeme ctx name <> lparen <> align (hcat (punctuate commaSpace $ map (ppDecl ctx) params)) <> rparen
 
-ppDecl :: Context -> Decl (Lexeme Name) -> Doc
+ppDecl :: Context -> Decl (Lexeme Name) -> Doc ()
 ppDecl ctx = \case
     Namespace name mems ->
         nest 2 (
-            text "namespace" <+> text (Text.unpack (Text.intercalate "_" name)) <+> lbrace <$>
+            pretty "namespace" <+> pretty (Text.unpack (Text.intercalate (Text.pack " ") name)) <+> lbrace <$$>
             vcat (map (ppDecl (ctx ++ name)) mems)
-        ) <$> rbrace
+        ) <$$> rbrace
 
     ClassDecl name mems ->
         nest 2 (
-            text "class" <+> ppLexeme ctx name <+> lbrace <$>
+            pretty "class" <+> ppLexeme ctx name <+> lbrace <$$>
             vcat (map (ppDecl ctx) mems)
-        ) <$> rbrace <> semi
+        ) <$$> rbrace <> semi
 
     Enumeration funs name mems ->
         nest 2 (
-            text "enum" <+> ppLexeme ctx name <+> lbracket <>
+            pretty "enum" <+> ppLexeme ctx name <+> lbracket <>
             hcat (punctuate commaSpace (map ppGenerated funs))
-            <> rbracket <+> lbrace <$>
+            <> rbracket <+> lbrace <$$>
             vcat (map (ppDecl ctx) mems)
-        ) <$> rbrace <> semi
+        ) <$$> rbrace <> semi
 
     Property name prop ->
         nest 2 (
-            text "property" <+> ppLexeme ctx name <+> colon <+> ppDecl ctx prop
-        ) <$> rbrace <> semi
+            pretty "property" <+> ppLexeme ctx name <+> colon <+> ppDecl ctx prop
+        ) <$$> rbrace <> semi
     ValueProp valType valGet valSet ->
-        ppDecl ctx valType <+> lbrace <$>
+        ppDecl ctx valType <+> lbrace <$$>
         vcat (map (ppDecl ctx) (maybeToList valGet ++ maybeToList valSet))
     ArrayProp arrType arrGet arrSet arrSize ->
-        ppDecl ctx arrType <+> lbrace <$>
+        ppDecl ctx arrType <+> lbrace <$$>
         vcat (map (ppDecl ctx) (maybeToList arrGet ++ maybeToList arrSet ++ maybeToList arrSize))
 
     Method constness ret name params ->
-        text "method" <+> ppDecl ctx ret <+> ppFunction ctx name params <+> ppConstness constness <> semi
+        pretty "method" <+> ppDecl ctx ret <+> ppFunction ctx name params <+> ppConstness constness <> semi
     Function ret name params ->
-        text "function" <+> ppDecl ctx ret <+> ppFunction ctx name params <> semi
+        pretty "function" <+> ppDecl ctx ret <+> ppFunction ctx name params <> semi
     Constructor name params ->
-        text "constructor" <+> ppFunction ctx name params <> semi
+        pretty "constructor" <+> ppFunction ctx name params <> semi
     Destructor name params ->
-        text "destructor" <+> ppFunction ctx name params <> semi
+        pretty "destructor" <+> ppFunction ctx name params <> semi
     CallbackTypeDecl name params ->
-        text "callback" <+> ppFunction ctx name params <> semi
-    IdTypeDecl name -> text "typedef uint32_t" <+> ppLexeme ctx name
-    TypeDecl name -> "typedef struct" <+> ppLexeme ctx name <+> ppLexeme ctx name <> semi
+        pretty "callback" <+> ppFunction ctx name params <> semi
+    IdTypeDecl name -> pretty "typedef uint32_t" <+> ppLexeme ctx name
+    TypeDecl name -> pretty "typedef struct" <+> ppLexeme ctx name <+> ppLexeme ctx name <> semi
 
     Var ty name ->
         ppDecl ctx ty <+> ppLexeme ctx name
     Define name ->
-        text "const" <+> ppLexeme ctx name <> semi
+        pretty "const" <+> ppLexeme ctx name <> semi
 
     Typename name -> ppLexeme ctx name
     EnumMember name -> ppLexeme ctx name <> comma
     BuiltinType ty -> ppBuiltinType ty
     CallbackType ty -> ppLexeme ctx ty
-    PointerType ty -> ppLexeme ctx ty <> char '*'
-    ConstPointerType ty -> text "const" <+> ppLexeme ctx ty <> char '*'
+    PointerType ty -> ppLexeme ctx ty <> pretty '*'
+    ConstPointerType ty -> pretty "const" <+> ppLexeme ctx ty <> pretty '*'
     SizedArrayType ty name -> ppDecl ctx ty <> lbracket <> ppDecl ctx name <> rbracket
-    ArrayType ty -> ppBuiltinType ty <> lbracket <> char '?' <> rbracket
-    UserArrayType ty -> ppLexeme ctx ty <> lbracket <> char '?' <> rbracket
-    ConstArrayType ty -> "const" <+> ppBuiltinType ty <> lbracket <> char '?' <> rbracket
-    ConstType ty -> text "const" <+> ppDecl ctx ty
+    ArrayType ty -> ppBuiltinType ty <> lbracket <> pretty '?' <> rbracket
+    UserArrayType ty -> ppLexeme ctx ty <> lbracket <> pretty '?' <> rbracket
+    ConstArrayType ty -> pretty "const" <+> ppBuiltinType ty <> lbracket <> pretty '?' <> rbracket
+    ConstType ty -> pretty "const" <+> ppDecl ctx ty
 
     Paren expr -> lparen <> ppDecl ctx expr <> rparen
     Ref name -> ppLexeme ctx name
     IntVal val -> ppLexeme ctx val
-    Abs e -> text "abs" <> lparen <> ppDecl ctx e <> rparen
-    Max a b -> text "max" <> lparen <> ppDecl ctx a <> comma <+> ppDecl ctx b <> rparen
-    Add l r -> ppDecl ctx l <+> char '+' <+> ppDecl ctx r
-    Sub l r -> ppDecl ctx l <+> char '-' <+> ppDecl ctx r
-    Mul l r -> ppDecl ctx l <+> char '*' <+> ppDecl ctx r
-    Div l r -> ppDecl ctx l <+> char '/' <+> ppDecl ctx r
+    Abs e -> pretty "abs" <> lparen <> ppDecl ctx e <> rparen
+    Max a b -> pretty "max" <> lparen <> ppDecl ctx a <> comma <+> ppDecl ctx b <> rparen
+    Add l r -> ppDecl ctx l <+> pretty '+' <+> ppDecl ctx r
+    Sub l r -> ppDecl ctx l <+> pretty '-' <+> ppDecl ctx r
+    Mul l r -> ppDecl ctx l <+> pretty '*' <+> ppDecl ctx r
+    Div l r -> ppDecl ctx l <+> pretty '/' <+> ppDecl ctx r
 
-ppGenerated :: Generated -> Doc
-ppGenerated GeneratedToString = text "to_string"
-ppGenerated GeneratedFromInt  = text "from_int"
+ppGenerated :: Generated -> Doc ()
+ppGenerated GeneratedToString = pretty "to_string"
+ppGenerated GeneratedFromInt  = pretty "from_int"
 
-ppConstness :: Constness -> Doc
-ppConstness ConstThis   = text "const"
-ppConstness MutableThis = text "mutable"
+ppConstness :: Constness -> Doc ()
+ppConstness ConstThis   = pretty "const"
+ppConstness MutableThis = pretty "mutable"
 
-ppBuiltinType :: BuiltinType -> Doc
-ppBuiltinType Void      = text "void"
-ppBuiltinType VoidPtr   = text "void*"
-ppBuiltinType Bool      = text "bool"
-ppBuiltinType Char      = text "char"
-ppBuiltinType (SInt bs) = text "int" <> ppBitSize bs <> text "_t"
-ppBuiltinType (UInt bs) = text "uint" <> ppBitSize bs <> text "_t"
-ppBuiltinType SizeT     = text "size_t"
-ppBuiltinType String    = text "string"
+ppBuiltinType :: BuiltinType -> Doc ()
+ppBuiltinType Void      = pretty "void"
+ppBuiltinType VoidPtr   = pretty "void*"
+ppBuiltinType Bool      = pretty "bool"
+ppBuiltinType Char      = pretty "pretty"
+ppBuiltinType (SInt bs) = pretty "int" <> ppBitSize bs <> pretty "_t"
+ppBuiltinType (UInt bs) = pretty "uint" <> ppBitSize bs <> pretty "_t"
+ppBuiltinType SizeT     = pretty "size_t"
+ppBuiltinType String    = pretty "string"
 
-ppBitSize :: BitSize -> Doc
+ppBitSize :: BitSize -> Doc ()
 ppBitSize B8  = int 8
 ppBitSize B16 = int 16
 ppBitSize B32 = int 32
 ppBitSize B64 = int 64
 
-ppMaybe :: (a -> Doc) -> Maybe a -> Doc
-ppMaybe _ Nothing  = empty
+ppMaybe :: (a -> Doc ()) -> Maybe a -> Doc ()
+ppMaybe _ Nothing  = mempty
 ppMaybe f (Just x) = f x
 
-ppLexeme :: Context -> Lexeme Name -> Doc
-ppLexeme ctx = text . nonEmpty . (if alwaysNamespace then display else displayWithin ctx) . lexemeText
+ppLexeme :: Context -> Lexeme Name -> Doc ()
+ppLexeme ctx = pretty . nonEmpty . (if alwaysNamespace then display else displayWithin ctx) . lexemeText
   where
     nonEmpty "" = "this"
     nonEmpty t  = t
 
-renderS :: Doc -> String
-renderS = flip displayS "" . renderSmart 1 120
+renderSmart :: Float -> Int -> Doc () -> SimpleDocStream ()
+renderSmart ribbonFraction widthPerLine
+    = layoutSmart LayoutOptions
+        { layoutPageWidth = AvailablePerLine widthPerLine (realToFrac ribbonFraction) }
 
-render :: Doc -> Text
-render = Text.pack . renderS
+renderS :: Doc () -> String
+renderS = Text.unpack . render
+
+render :: Doc () -> Text
+render = TL.toStrict . Term.renderLazy . renderSmart 1 120
 
 generate :: Model (Lexeme Name) -> Text
 generate = render . ppModel
+
+infixr 5 <$$>
+(<$$>) :: Doc a -> Doc a -> Doc a
+x <$$> y = x <> line <> y
+
+int :: Int -> Doc ()
+int = pretty
--- src/Apigen/Language/PyDsl.hs
+++ src/Apigen/Language/PyDsl.hs
@@ -1,53 +1,55 @@
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE Strict            #-}
-{- HLINT ignore "Functor law" -}
-{- HLINT ignore "Use <$" -}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE Strict     #-}
 module Apigen.Language.PyDsl (generate) where
 
-import           Apigen.Parser.SymbolTable    (Name)
-import           Apigen.Types                 (BitSize (..), BuiltinType (..),
-                                               Constness (..), Decl (..),
-                                               Generated (..), Model (..),
-                                               Module (..))
-import           Data.Text                    (Text)
-import qualified Data.Text                    as Text
-import           Language.Cimple              (Lexeme (..), LexemeClass)
-import           Prelude                      hiding ((<$>))
-import           Text.PrettyPrint.ANSI.Leijen
+import           Apigen.Parser.SymbolTable (Name)
+import           Apigen.Types              (BitSize (..), BuiltinType (..),
+                                            Constness (..), Decl (..),
+                                            Generated (..), Model (..),
+                                            Module (..))
+import           Data.Text                 (Text)
+import qualified Data.Text                 as Text
+import qualified Data.Text.Lazy            as TL
+import           Language.Cimple           (Lexeme (..), LexemeClass)
+import           Prelude                   hiding ((<$>))
+import           Prettyprinter
+import           Prettyprinter.Render.Text as Term
 
-commaSpace :: Doc
+commaSpace :: Doc ()
 commaSpace = comma <> softline
 
-commaSep :: [Doc] -> [Doc]
+commaSep :: [Doc ()] -> [Doc ()]
 commaSep = punctuate commaSpace
 
-go :: ([Doc] -> Doc) -> String -> [Doc] -> Doc
+go :: ([Doc ()] -> Doc ()) -> String -> [Doc ()] -> Doc ()
 go f cls mems = ppCtor cls <> lparen <> f (commaSep mems) <> rparen
 
-ppCtor :: String -> Doc
-ppCtor = text . ("apigen." <>)
+ppCtor :: String -> Doc ()
+ppCtor = pretty . ("apigen." <>)
 
-hgo :: String -> [Doc] -> Doc
+hgo :: String -> [Doc ()] -> Doc ()
 hgo = go hcat
 
-vgo :: String -> [Doc] -> Doc
+vgo :: String -> [Doc ()] -> Doc ()
 vgo = go vcat
 
-ppList :: (a -> Doc) -> [a] -> Doc
-ppList pp l = lbracket <//> hcat (commaSep (map pp l)) <> rbracket
+linebreak :: Doc ()
+linebreak = group (flatAlt line mempty)
 
-ppModel :: Model (Lexeme Name) -> Doc
+ppList :: (a -> Doc ()) -> [a] -> Doc ()
+ppList pp l = lbracket <> linebreak <> hcat (commaSep (map pp l)) <> rbracket
+
+ppModel :: Model (Lexeme Name) -> Doc ()
 ppModel (Model mods) =
     vgo "Model" [ppList ppModule mods] <> line
 
-ppModule :: Module (Lexeme Name) -> Doc
+ppModule :: Module (Lexeme Name) -> Doc ()
 ppModule (Module file decls) =
-    vgo "Module" ["\"" <> text file <> "\"", ppList ppDecl decls]
+    vgo "Module" [pretty "\"" <> pretty file <> pretty "\"", ppList ppDecl decls]
 
-ppDecl :: Decl (Lexeme Name) -> Doc
+ppDecl :: Decl (Lexeme Name) -> Doc ()
 ppDecl = \case
-    Namespace name mems                     -> vgo "Namespace" [ppList (text . show) name, ppList ppDecl mems]
+    Namespace name mems                     -> vgo "Namespace" [ppList (pretty . show) name, ppList ppDecl mems]
     ClassDecl name mems                     -> vgo "ClassDecl" [ppLexeme name, ppList ppDecl mems]
     Enumeration funs name mems              -> vgo "Enumeration" [ppList ppGenerated funs, ppLexeme name, ppList ppDecl mems]
     Property name prop                      -> hgo "Property" [ppLexeme name, ppDecl prop]
@@ -83,14 +85,14 @@ ppDecl = \case
     Mul l r                                 -> hgo "Mul" [ppDecl l, ppDecl r]
     Div l r                                 -> hgo "Div" [ppDecl l, ppDecl r]
 
-ppConstness :: Constness -> Doc
-ppConstness ConstThis   = text "True"
-ppConstness MutableThis = text "False"
+ppConstness :: Constness -> Doc ()
+ppConstness ConstThis   = pretty "True"
+ppConstness MutableThis = pretty "False"
 
-ppGenerated :: Generated -> Doc
+ppGenerated :: Generated -> Doc ()
 ppGenerated = ppCtor . ("Generated." <>) . show
 
-ppBuiltinType :: BuiltinType -> Doc
+ppBuiltinType :: BuiltinType -> Doc ()
 ppBuiltinType Void      = ppCtor "Void"
 ppBuiltinType VoidPtr   = ppCtor "VoidPtr"
 ppBuiltinType Bool      = ppCtor "Bool"
@@ -100,31 +102,37 @@ ppBuiltinType (UInt bs) = hgo "UInt" [ppBitSize bs]
 ppBuiltinType SizeT     = ppCtor "SizeT"
 ppBuiltinType String    = ppCtor "String"
 
-ppBitSize :: BitSize -> Doc
+ppBitSize :: BitSize -> Doc ()
 ppBitSize B8  = int 8
 ppBitSize B16 = int 16
 ppBitSize B32 = int 32
 ppBitSize B64 = int 64
 
-ppMaybe :: (a -> Doc) -> Maybe a -> Doc
-ppMaybe _ Nothing  = text "None"
+ppMaybe :: (a -> Doc ()) -> Maybe a -> Doc ()
+ppMaybe _ Nothing  = pretty "None"
 ppMaybe f (Just x) = f x
 
-ppLexeme :: Lexeme Name -> Doc
+ppLexeme :: Lexeme Name -> Doc ()
 ppLexeme (L _ c s) = ppName c s
 
-ppName :: LexemeClass -> Name -> Doc
+ppName :: LexemeClass -> Name -> Doc ()
 ppName c (ns, name) = hgo "Name"
     [ ppCtor . ("LexemeClass." <>) . show $ c
-    , ppList (text . show) ns
-    , ppList (text . show) name
+    , ppList (pretty . show) ns
+    , ppList (pretty . show) name
     ]
 
-renderS :: Doc -> String
-renderS = flip displayS "" . renderSmart 1 120
 
-render :: Doc -> Text
-render = Text.pack . renderS
+renderSmart :: Float -> Int -> Doc () -> SimpleDocStream ()
+renderSmart ribbonFraction widthPerLine
+    = layoutSmart LayoutOptions
+        { layoutPageWidth = AvailablePerLine widthPerLine (realToFrac ribbonFraction) }
+
+render :: Doc () -> Text
+render = TL.toStrict . Term.renderLazy . renderSmart 1 120
 
 generate :: Model (Lexeme Name) -> Text
 generate = render . ppModel
+
+int :: Int -> Doc ()
+int = pretty
